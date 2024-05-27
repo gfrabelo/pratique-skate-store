a3ea22ce492f0b47c0070f0f8b0e28620e60cca8
gfrabelo <gabrielrabelob@gmail.com>
2024-05-27 16:32:00 -0300

    feat: adiciona icone Bt comprar

--- src/app/components/AddCart.tsx
+++ src/app/components/AddCart.tsx
@@ -7,6 +7,9 @@ export default function Product({ product }: { product: ProductType }) {
     const { addToCart } = userCartStore();
 
     return (
-        <button onClick={() => addToCart(product)} className="product-box__button">Comprar</button>
+        <button onClick={() => addToCart(product)} className="product-box__button">
+            Comprar
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="icon-bt"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 12H20M20 12L16 8M20 12L16 16" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+        </button>
     )
 }
